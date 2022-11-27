625e9a3c1b774fb761650ab0cb479c3758c6088e
Zu18 <zuhra.galieva.2014@gmail.com>
2022-11-27 09:30:08 +0000

    Add the file 'recursion_array.js'

---
+++ recursion_array.js
@@ -0,0 +1,11 @@
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+// should display [1, 2, 3, 4, 5]
