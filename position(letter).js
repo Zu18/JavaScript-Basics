6a6f3a8679db65116941f35aeedd338dd37ffb69
Zukhra <zuhra.galieva.2014@gmail.com>
2023-06-21 21:17:06 +0100

    Add the file position(letter).js

---
+++ position(letter).js
@@ -0,0 +1,4 @@
+function position(letter){
+  let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  return "Position of alphabet: " + (letters.indexOf(letter) + 1);
+}
